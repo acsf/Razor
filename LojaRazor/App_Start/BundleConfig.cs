0081e70239019ef2b7e4df191cc5e91a17f430dd
acsf <a.carlossf@hotmail.com>
2022-05-20 21:15:38 -0300

    Otimizacao das views

--- LojaRazor/App_Start/BundleConfig.cs
+++ LojaRazor/App_Start/BundleConfig.cs
@@ -8,7 +8,16 @@ namespace LojaRazor
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            StyleBundle estilos = new StyleBundle("~/bundles/estilos");
+            estilos.Include("~/Content/bootstrap/css/bootstrap.css");
+            estilos.Include("~/Content/Style.css");
+            bundles.Add(estilos);
+
+            ScriptBundle script = new ScriptBundle("~/bundles/scripts");
+            script.Include("~/Scripts/jquery-1.7.1.js");
+            script.Include("~/Scripts/jquery.validate.js");
+            script.Include("~/Scripts/jquery.validate.unobtrusive.js");
+            bundles.Add(script);
         }
     }
 }
